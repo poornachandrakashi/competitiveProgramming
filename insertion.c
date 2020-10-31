dd2bfc7bc0889d9f3fe9591f2bca3da7c51594b0
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-11-01 02:30:45 +0530

    Adding insertion sort

---
+++ insertion.c
@@ -0,0 +1,21 @@
+//Insertion sort using C program
+#include<stdio.h>
+
+int main(){
+  int arr[9] = {9,6,5,0,8,2,7,1,3};
+  int key = 0;
+  int i,j;
+  for(j=1;j<9;j++){
+    i=j-1;
+    key = arr[j];
+    while(i>-1 && arr[i]>key){
+      arr[i+1] = arr[i];
+      i--;
+    }
+    arr[i+1] = key;
+  }
+
+  for(i=0;i<9;i++){
+    printf("%d",arr[i]);
+  }
+}
